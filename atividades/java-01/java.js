7f13d59f0deaa0086a8d347428a6056ce3285559
AnaClaradeJesus <90980094+AnaClaradeJesus@users.noreply.github.com>
2021-10-26 19:23:31 -0300

    Add files via upload

---
+++ atividades/java-01/java.js
@@ -0,0 +1,6 @@
+let nome= prompt('Digite seu nome:')
+let ano_nascimento= prompt ('digite seu ano de nascimento:')
+let conta= 2021-ano_nascimento
+
+let mensagem =`olá seu nome é ${nome}, e você tem ${conta} anos!`
+alert(mensagem)
