574208b568024d9b1babaa147f1016ffd6a1269a
AnaClaradeJesus <90980094+AnaClaradeJesus@users.noreply.github.com>
2021-11-06 21:22:46 -0300

    Add files via upload

---
+++ atividades/comando-de-repeticao/repetir.js
@@ -0,0 +1,11 @@
+    num1 = parseInt( prompt ("Digite o primeiro número: ") );
+    num2 = parseInt( prompt ("Digite o segundo número: ") );
+    num3 = parseInt( prompt ("Digite o terceiro número: ") );
+    num4 = parseInt( prompt ("Digite o quarto número: ") );
+
+    calculo1 = (num1+ num2 + num3 + num4);
+    calculo2 = calculo1/4;
+    
+    alert ("A soma dos valores é: " + calculo1);
+   alert ("A média dos valores é: " + calculo2);
+
