41b37f9bbccb84e4fb4518734975ac619a24bc61
AnaClaradeJesus <90980094+AnaClaradeJesus@users.noreply.github.com>
2021-11-24 23:18:29 -0300

    Add files via upload

---
+++ atividades/projeto final/ppmf.js
@@ -0,0 +1,6 @@
+let nome= prompt('Digite seu nome:')
+let ano_nascimento= prompt ('digite seu ano de nascimento:')
+let conta= 2021-ano_nascimento
+
+let mensagem =`olá ${nome}, você tem ${conta} anos e ficou esse tempo todo sem ver esse site. Fico muito feliz de te receber na minha página.Espero que aproveite seu tempo por aki!`
+alert(mensagem)
